23a835eccce25ddf652654c24f1ba561140c92ed
Fluxxx1k <tmpsavvarusgames@gmail.com>
2025-10-05 19:19:11 +0300

    Add files via upload
    File with names like "BLACK", "ANARCHIST".
    It's for avoiding typos.
    Need in versions SH_2_2_6.
    (But here was upload only SH2_2_4- and SH2_2_7+,
    So it need only in SH2_2_7+)

---
+++ standart_names.py
@@ -0,0 +1,21 @@
+class X:
+    """
+    Class for special names
+    """
+    CHANCELLOR = CNC = "chancellor"
+    PRESIDENT = PRS = "president"
+    HITLER = HTLR = "HITLER"
+    STALIN = STLN = "STALIN"
+    NO_INFO = N_I = "No Info"
+    RED = "RED"
+    BLACK = BLK = "BLACK"
+    MOLOTOV = MOL = "MOLOTOV"
+    RIBBENTROP = RIB = "RIBBENTROP"
+    ANARCHIST = NRH = "NRH"  # "ANARCHIST"
+    OLD = "OLD"  # For "legacy"
+    HTML = "HTML"
+    GULAG = "GULAG"
+    SHOUT = "SHOUT"
+    KILLED = "KILLED"
+# class HTML(str):
+#     pass
