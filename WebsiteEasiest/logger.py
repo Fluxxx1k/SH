0e19247fc97a9870954fa494f3bd9f789e886183
root <flxflxflux@gmail.com>
2026-01-29 07:31:04 +0000

    feat(logger): add low_debug level and improve logging functionality Nd coloring
    - Add new LOW_DEBUG level with value 5, lower than DEBUG=10
    - Implement low_debug method for loggers
    - Add LowDebugFilter class to filter LOW_DEBUG level logs
    - Update ColoredFormatterFile and ColoredFormatterConsole to support LOW_DEBUG level
    - Add low_debug_handler for handling LOW_DEBUG level logs
    - Improve error handling and compability mode for color imports
    - Update existing log messages formatting
     - Using more bright colors

--- WebsiteEasiest/logger.py
+++ WebsiteEasiest/logger.py
@@ -1,25 +1,28 @@
 import logging
 import sys, os
 from logging.handlers import RotatingFileHandler
+from typing import Callable
+
 try:
     import cli.colors as color
 except Exception as e:
-    print(f"Could not import colors: {repr(e)}")
+    print(f"Could not import colors: {repr(e)}, using compability mode")
     class color:
         GRAY_BACKGROUND = "\033[100m"
         WHITE_BACKGROUND_BRIGHT = "\033[107m"
         YELLOW_BACKGROUND = "\033[43m"
         YELLOW_BACKGROUND_BRIGHT = "\033[103m"
         RED_BACKGROUND = "\033[41m"
-        RED_BACKGROUND_BRIGHT = "\033[101m"
+        RED_BACKGROUND_VERY_BRIGHT = "\033[101m"
         END = '\033[0m'
         RESET_TEXT = '\033[39m'
-        GREY_TEXT = '\033[90m'
+        GRAY_TEXT = '\033[90m'
         BLACK_TEXT = '\033[30m'
         RED_TEXT = '\033[31m'
-        RED_TEXT_BRIGHT = '\033[91m'
+        RED_TEXT_VERY_BRIGHT = '\033[91m'
         YELLOW_TEXT = '\033[33m'
         YELLOW_TEXT_BRIGHT = '\033[93m'
+        YELLOW_TEXT_VERY_BRIGHT = '\033[93m'
         BOLD = '\033[1m'
         UNDERLINE = '\033[4m'
 
@@ -27,12 +30,10 @@ try:
     is_server = os.uname().nodename == "SERVERNYA"
 except AttributeError:
     is_server = True
-# Создание логгера
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
+    
 log_text = "%(asctime)s - %(message)s"
 log_dir = os.path.join(os.path.dirname(__file__),"logger_logs")
+low_debug_path = os.path.join(log_dir, "low_debug.log")
 debug_path = os.path.join(log_dir, "debug.log")
 info_path = os.path.join(log_dir, "info.log")
 warning_path = os.path.join(log_dir, "warning.log")
@@ -43,6 +44,14 @@ warning_error_fatal_path = os.path.join(log_dir, "warning_error_fatal.log")
 
 if not os.path.exists(log_dir):
     os.makedirs(log_dir)
+
+# 1. Определяем новый уровень (значение 5, ниже DEBUG=10)
+LOW_DEBUG_NUM = 5
+logging.addLevelName(LOW_DEBUG_NUM, "LOW_DEBUG")
+
+class LowDebugFilter(logging.Filter):
+    def filter(self, record):
+        return record.levelno == LOW_DEBUG_NUM
 
 class DebugFilter(logging.Filter):
     def filter(self, record):
@@ -65,56 +74,72 @@ class FatalFilter(logging.Filter):
         return logging.ERROR < record.levelno
 
 
+# 2. Функция для добавления метода low_debug() к логгерам
+def low_debug(self, message, *args, **kwargs):
+    if self.isEnabledFor(LOW_DEBUG_NUM):
+        self._log(LOW_DEBUG_NUM, message, args, **kwargs)
+
+# 3. Добавляем метод в класс Logger
+logging.Logger.low_debug = low_debug
+
+# 4. Создаем логгер и настраиваем
+logger = logging.getLogger("my_logger")
+logger.setLevel(LOW_DEBUG_NUM)  # Ловим события от LOW_DEBUG и выше
+logger.low_debug: Callable
+
 class ColoredFormatterFile(logging.Formatter):
     """
     Logging Formatter to add colors to log messages
     """
-    try:
-        grey = color.GREY_TEXT
-        standard = color.RESET_TEXT
-        yellow = color.YELLOW_TEXT
-        error = color.RED_TEXT
-        critical = color.RED_TEXT_BRIGHT + color.BOLD + color.UNDERLINE
-        reset = color.END
-    except Exception as e:
-        grey = "\x1b[90m"
-        standard = "\x1b[39m"
-        yellow = "\x1b[93m"
-        error = "\x1b[91m"
-        critical = "\x1b[91;1;4m"
-        reset = "\x1b[0m"
     format = "%(asctime)s - %(message)s"
 
     FORMATS = {
-        logging.DEBUG: f'{color.GRAY_BACKGROUND if is_server else ""}DEBUG{color.END if is_server else ""}   | {color.GREY_TEXT if is_server else ""}{log_text}{color.END if is_server else ""}',
-        logging.INFO: f'{color.WHITE_BACKGROUND_BRIGHT+color.BLACK_TEXT if is_server else ""}INFO{color.END if is_server else ""}    | {log_text}{color.END if is_server else ""}',
-        logging.WARNING: f'{color.YELLOW_BACKGROUND_BRIGHT if is_server else ""}{color.RED_TEXT if is_server else ""}WARNING{color.END if is_server else ""} | {color.YELLOW_TEXT_BRIGHT if is_server else ""}{log_text}{color.END if is_server else ""}',
-        logging.ERROR: f'{color.RED_BACKGROUND if is_server else ""}{color.BLACK_TEXT if is_server else ""}ERROR{color.END}   | {color.RED_TEXT if is_server else ""}{log_text}{color.END if  is_server else ""} {{%(filename)s - %(funcName)s - %(lineno)d}}',
-        logging.CRITICAL: f'{color.BOLD+color.UNDERLINE+color.RED_BACKGROUND_BRIGHT+color.YELLOW_TEXT_BRIGHT if is_server else ""}FATAL{color.END if is_server else ""}   | {color.RED_TEXT_BRIGHT if is_server else ""}{log_text}{color.END if is_server else ""} {{%(filename)s - %(funcName)s - %(lineno)d}}',
+        LOW_DEBUG_NUM: f'{color.GRAY_DARK_BACKGROUND+color.BLACK_TEXT if is_server else ""}LOW_DEBUG{color.END if is_server else ""} | {color.GRAY_DARK_TEXT if is_server else ""}{log_text}{color.END if is_server else ""}',
+        logging.DEBUG: f'{color.GRAY_BACKGROUND if is_server else ""}DEBUG{color.END if is_server else ""}     | {color.GRAY_TEXT if is_server else ""}{log_text}{color.END if is_server else ""}',
+        logging.INFO: f'{color.WHITE_BACKGROUND_BRIGHT+color.BLACK_TEXT if is_server else ""}INFO{color.END if is_server else ""}      | {log_text}{color.END if is_server else ""}',
+        logging.WARNING: f'{color.YELLOW_BACKGROUND_BRIGHT if is_server else ""}{color.RED_TEXT if is_server else ""}WARNING{color.END if is_server else ""}   | {color.YELLOW_TEXT_BRIGHT if is_server else ""}{log_text}{color.END if is_server else ""}',
+        logging.ERROR: f'{color.RED_BACKGROUND if is_server else ""}{color.BLACK_TEXT if is_server else ""}ERROR{color.END}     | {color.RED_TEXT if is_server else ""}{log_text}{color.END if  is_server else ""} {{%(filename)s - %(funcName)s - %(lineno)d}}',
+        logging.CRITICAL: f'{color.BOLD+color.UNDERLINE+color.RED_BACKGROUND_VERY_BRIGHT+color.YELLOW_TEXT_VERY_BRIGHT if is_server else ""}FATAL{color.END if is_server else ""}     | {color.RED_TEXT_VERY_BRIGHT if is_server else ""}{log_text}{color.END if is_server else ""} {{%(filename)s - %(funcName)s - %(lineno)d}}',
     }
 
     def format(self, record):
         log_fmt = self.FORMATS.get(record.levelno)
         if log_fmt is None and record.levelno !=  logging.WARNING:
             logger.warning(f"Unknown log level: {record.levelno}")
+            import pprint
+            pprint.pprint(self.FORMATS)
             log_fmt = self.FORMATS[logging.WARNING]
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
 
 class ColoredFormatterConsole(ColoredFormatterFile):
     FORMATS = {
-        logging.DEBUG: f'{color.GRAY_BACKGROUND}DEBUG{color.END}   | {color.GREY_TEXT}{log_text}{color.END}',
-        logging.INFO: f'{color.WHITE_BACKGROUND_BRIGHT + color.BLACK_TEXT}INFO{color.END}    | {log_text}{color.END}',
-        logging.WARNING: f'{color.YELLOW_BACKGROUND_BRIGHT + color.RED_TEXT}WARNING{color.END} | {color.YELLOW_TEXT_BRIGHT}{log_text}{color.END}',
-        logging.ERROR: f'{color.RED_BACKGROUND + color.BLACK_TEXT}ERROR{color.END}   | {color.RED_TEXT}{log_text}{color.END} {{%(filename)s - %(funcName)s - %(lineno)d}}',
-        logging.CRITICAL: f'{color.BOLD + color.UNDERLINE + color.RED_BACKGROUND_BRIGHT + color.YELLOW_TEXT_BRIGHT}FATAL{color.END}   | {color.RED_TEXT_BRIGHT}{log_text}{color.END} {{%(filename)s - %(funcName)s - %(lineno)d}}',
+        LOW_DEBUG_NUM: f'{color.GRAY_DARK_BACKGROUND+color.BLACK_TEXT}LOW_DEBUG{color.END} | {color.GRAY_DARK_TEXT}{log_text}{color.END}',
+        logging.DEBUG: f'{color.GRAY_BACKGROUND}DEBUG{color.END}     | {color.GRAY_TEXT}{log_text}{color.END}',
+        logging.INFO: f'{color.WHITE_BACKGROUND_BRIGHT + color.BLACK_TEXT}INFO{color.END}      | {log_text}{color.END}',
+        logging.WARNING: f'{color.YELLOW_BACKGROUND_BRIGHT + color.RED_TEXT}WARNING{color.END}   | {color.YELLOW_TEXT_BRIGHT}{log_text}{color.END}',
+        logging.ERROR: f'{color.RED_BACKGROUND + color.BLACK_TEXT}ERROR{color.END}     | {color.RED_TEXT}{log_text}{color.END} {{%(filename)s - %(funcName)s - %(lineno)d}}',
+        logging.CRITICAL: f'{color.BOLD + color.UNDERLINE + color.RED_BACKGROUND_VERY_BRIGHT + color.YELLOW_TEXT_VERY_BRIGHT}FATAL{color.END}     | {color.RED_TEXT_VERY_BRIGHT}{log_text}{color.END} {{%(filename)s - %(funcName)s - %(lineno)d}}',
     }
 
 all_handler = logging.StreamHandler(sys.stdout)
-all_handler.setLevel(logging.DEBUG)
+all_handler.setLevel(LOW_DEBUG_NUM)
 all_handler.setFormatter(ColoredFormatterConsole())
 logger.addHandler(all_handler)
 
+
+low_debug_handler = RotatingFileHandler(
+maxBytes=5<<20,  # 5MB
+    backupCount=3,
+    mode='a',
+    filename=low_debug_path,
+    encoding='utf-8')
+low_debug_handler.setLevel(LOW_DEBUG_NUM)
+low_debug_handler.addFilter(LowDebugFilter())
+low_debug_handler.setFormatter(logging.Formatter(
+    ColoredFormatterFile.FORMATS[LOW_DEBUG_NUM]
+))
+logger.addHandler(low_debug_handler)
 
 debug_file_handler = RotatingFileHandler(
 maxBytes=5<<20,  # 5MB
@@ -208,7 +233,7 @@ logger.addHandler(warning_error_fatal_path_file_handler)
 logger.debug("Logger created and loaded")
 
   # Testing logs
-
+logger.low_debug("Low Debug log")
 logger.debug("Debug log")
 logger.info("Info log")
 logger.warning("Warning log")
