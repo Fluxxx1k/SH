01a3e2762a02837d2a482b2ff77654f14e37bd91
Fluxxx1k <tmpsavvarusgames@gmail.com>
2026-02-02 11:22:46 +0300

    feat(data): add game logs directory
    - Introduce new directory for storing game logs
    - Ensure logs directory is created if it doesn't exist
    - Update debug information to include new logs path

--- WebsiteEasiest/data/data_paths.py
+++ WebsiteEasiest/data/data_paths.py
@@ -5,8 +5,10 @@ dirname = os.path.dirname(__file__)
 
 path_games = os.path.join(dirname, 'games')
 path_existed_games = os.path.join(dirname, 'ended_games')
+path_logs_games = os.path.join(dirname, 'logs_games')
 os.makedirs(path_games, exist_ok=True)
 os.makedirs(path_existed_games, exist_ok=True)
+os.makedirs(path_logs_games, exist_ok=True)
 
 path_players = os.path.join(dirname, 'players')
 path_IP = os.path.join(dirname, 'IP')
@@ -21,6 +23,7 @@ logger.debug(f"Paths:"
              f"\n\t\t{dirname= },"
              f"\n\t\t{path_games= },"
              f"\n\t\t{path_existed_games= },"
+             f"\n\t\t{path_logs_games= },"
              f"\n\t\t{path_players= },"
              f"\n\t\t{path_IP= },"
              f"\n\t\t{path_banned= }")
