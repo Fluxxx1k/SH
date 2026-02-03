62f0b4df040ac0b0daa63da915976bc4d4b89bd1
root <flxflxflux@gmail.com>
2026-02-03 07:21:43 +0000

    feat(data): add new actions games directory
    - Create new directory for storing game actions
    - Update logging to include new path information
    - Additions:
      - path_actions_games = os.path.join(dirname, 'actions_games')
      - os.makedirs(path_actions_games, exist_ok=True)
      - logger.low_debug() replaced logger.debug() for more detailed logging

--- WebsiteEasiest/data/data_paths.py
+++ WebsiteEasiest/data/data_paths.py
@@ -6,9 +6,11 @@ dirname = os.path.dirname(__file__)
 path_games = os.path.join(dirname, 'games')
 path_existed_games = os.path.join(dirname, 'ended_games')
 path_logs_games = os.path.join(dirname, 'logs_games')
+path_actions_games = os.path.join(dirname, 'actions_games')
 os.makedirs(path_games, exist_ok=True)
 os.makedirs(path_existed_games, exist_ok=True)
 os.makedirs(path_logs_games, exist_ok=True)
+os.makedirs(path_actions_games, exist_ok=True)
 
 path_players = os.path.join(dirname, 'players')
 path_IP = os.path.join(dirname, 'IP')
@@ -18,12 +20,13 @@ os.makedirs(path_IP, exist_ok=True)
 path_banned = os.path.join(dirname, 'banned_IP.json')
 open(path_banned, 'a+', encoding='utf-8').close()
 
-logger.debug(f"Paths:"
+logger.low_debug(f"Paths:"
              f"\n\t\t{__file__= }"
              f"\n\t\t{dirname= },"
              f"\n\t\t{path_games= },"
              f"\n\t\t{path_existed_games= },"
              f"\n\t\t{path_logs_games= },"
+             f"\n\t\t{path_actions_games= },"
              f"\n\t\t{path_players= },"
              f"\n\t\t{path_IP= },"
              f"\n\t\t{path_banned= }")
